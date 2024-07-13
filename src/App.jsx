b629fca5b61c34b92607d2a1e8691b3f7c79fdf5
AL Riyad <imalriyad@gmail.com>
2024-07-13 12:34:13 +0600

    data fecthing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [cardData, setCardData] = useState([]);
+  const getCardData = async () => {
+    const data = await axios.get(
+      "https://react-interview.1putym.easypanel.host/api/product?email=naim.microdeft@gmail.com"
+    );
+    console.log(data.data);
+  };
+  getCardData();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="">
+        <img src="./" alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
